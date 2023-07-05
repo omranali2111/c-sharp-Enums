6a4695a7624a1a742713c0c7e7980e35fab9e726
omranali2111 <aljabri1234@hotmail.com>
2023-07-05 10:13:27 +0400

    using enums

---
+++ c-sharp-Enums/Program.cs
@@ -0,0 +1,10 @@
+﻿using c_sharp_Enums;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        User user1 = new User("omar", 123, Gender.MALE);
+      User user2 = new User("aisha", 123, Gender.FEMALE);
+    }
+}
---
+++ c-sharp-Enums/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_Enums
+{
+    internal class User
+    {
+        public string Name { get; set; }
+        public int id { get; set; }
+
+        public Gender gender { get; set; }
+
+        public User(string Name, int id, Gender gender) 
+        { 
+            this.Name = Name;
+            this.id = id;
+            this.gender = gender;
+        }
+    }
+
+    enum Gender
+    {
+        MALE,
+        FEMALE,
+        OTHER
+
+    }
+}
